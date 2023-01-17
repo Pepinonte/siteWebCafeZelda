2c0f98d812e6be8287a6f98df49cf23a2943ca6a
Gonni <nicolas.cros.sn@gmail.com>
2023-01-17 21:27:45 +0100

    NavBar Style ok  (Responsive PAS OK)

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,13 +1,29 @@
 import React, { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
+import "../css/navbar.css";
 
 const Navigation = (props) => {
   return (
     <div className="navigation">
-      <nav>
-        <NavLink to="/">home</NavLink>
+      <nav className="navbar">
+        <NavLink style={{ textDecoration: "none", color: "inherit" }} to="/">
+          <h1 className="caf-zelda jost-bold-white-32px">CAFÉ ZELDA</h1>
+        </NavLink>
+        <div className="navbar-1 montserrat-semi-bold-white-16px">
+          <NavLink
+            style={{ textDecoration: "none", color: "inherit" }}
+            to="/test"
+          >
+            <div className="navbar-link">QUI SOMMES-NOUS ?</div>
+          </NavLink>
 
-        <NavLink to="/test">test</NavLink>
+          <NavLink
+            style={{ textDecoration: "none", color: "inherit" }}
+            to="/test"
+          >
+            <div className="navbar-link">NOS ARTICLES</div>
+          </NavLink>
+        </div>
       </nav>
     </div>
   );
