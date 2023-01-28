9d37719dfda861a90e27c500d3d9abc1c4667065
Gonni <nicolas.cros.sn@gmail.com>
2023-01-28 11:44:11 +0100

    Responsive NAV BAR OK + installation fontawesome pour menu burger ma gueule

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,16 +1,28 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import "../css/navbar.css";
+import "font-awesome/css/font-awesome.min.css";
 
 const Navigation = (props) => {
+  const [navbarOpen, setNavbarOpen] = useState(false);
+
+  const handleNavbarToggle = () => {
+    setNavbarOpen(!navbarOpen);
+  };
+
   return (
     <div className="navigation">
       <nav className="navbar">
         <NavLink style={{ textDecoration: "none", color: "inherit" }} to="/">
           <h1 className="caf-zelda jost-bold-white-32px">CAFÉ ZELDA</h1>
         </NavLink>
-        <div className="navbar-1 montserrat-semi-bold-white-16px">
+        <div
+          className={`navbar-1 montserrat-semi-bold-white-16px ${
+            navbarOpen ? "open" : ""
+          }`}
+        >
           <NavLink
+            className="navlink"
             style={{ textDecoration: "none", color: "inherit" }}
             to="/LeCafeZelda"
           >
@@ -18,12 +30,19 @@ const Navigation = (props) => {
           </NavLink>
 
           <NavLink
+            className="navlink"
             style={{ textDecoration: "none", color: "inherit" }}
             to="/LesArticles"
           >
             <div className="navbar-link">NOS ARTICLES</div>
           </NavLink>
         </div>
+        <button className="navbar-toggle" onClick={handleNavbarToggle}>
+          <i
+            className={`fa fa-${navbarOpen ? "times" : "bars"}`}
+            aria-hidden="true"
+          ></i>
+        </button>
       </nav>
     </div>
   );
