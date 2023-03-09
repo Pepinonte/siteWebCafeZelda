c4e174ce7dd9b05c2e189e8cc01b06acc2eceac6
Gonni <nicolas.cros.sn@gmail.com>
2023-03-09 23:44:23 +0100

    Bonjour attend je mange tout comme séphiroth
    mis en place des titres, et des images de fond C TOUT

--- src/components/ArticleList.jsx
+++ src/components/ArticleList.jsx
@@ -1,15 +1,60 @@
 import React from "react";
 import LargeArticle from "./articlesCards/LargeArticle";
 import "../css/ArticleList.css";
+
+import fondTeletubbies from "../img/imgtest/fondwhut.jpg";
+import fondTetra from "../img/imgtest/Fanatique.jpg";
+import fondpasSwitch from "../img/imgtest/passwitch.png";
+import fondTiggle from "../img/imgtest/Tiggle.png";
+import fondCaca from "../img/imgtest/Caca.png";
+import fondMort from "../img/imgtest/Detruit.png";
+
 const ArticleList = () => {
   // On crée une liste d'articles en local
   const articles = [
-    { id: 1, title: "Titre de l'article 1", content: "Contenu de l'article 1" },
-    { id: 2, title: "Titre de l'article 2", content: "Contenu de l'article 2" },
-    { id: 3, title: "Titre de l'article 3", content: "Contenu de l'article 3" },
-    { id: 4, title: "Titre de l'article 4", content: "Contenu de l'article 4" },
-    { id: 5, title: "Titre de l'article 5", content: "Contenu de l'article 5" },
-    { id: 6, title: "Titre de l'article 6", content: "Contenu de l'article 6" },
+    {
+      id: 1,
+      title: "Analyses et théories sur le dernier épisode des Teletubies",
+      content: "bizarre le type",
+      autheur: "Legonde",
+      backgroundImage: fondTeletubbies,
+    },
+    {
+      id: 2,
+      title: "Officiel: Wind Waker HD sur Nintendo Switch n'existera pas !",
+      content: "Jvais me faire taper moi",
+      autheur: "Askip",
+      backgroundImage: fondpasSwitch,
+    },
+    {
+      id: 3,
+      title: "Le jour où je suis devenu le caca de LionRouge",
+      content: "ptdr hein",
+      autheur: "Marnie",
+      backgroundImage: fondCaca,
+    },
+    {
+      id: 4,
+      title: "Ivre il détruit ses travailleurs",
+      content: "jme suis fait bully",
+      autheur: "Gonni",
+      backgroundImage: fondMort,
+    },
+    {
+      id: 5,
+      title: "Je suis terrifié à l'idée même que Tingle existe",
+      content: "le boug",
+      autheur: "Nebu",
+
+      backgroundImage: fondTiggle,
+    },
+    {
+      id: 6,
+      title: "Une fanatique de Wind Waker harcèle Nintendo sur Twitter",
+      content: "ça va faire 1 an mdr",
+      autheur: "Lion Rouge",
+      backgroundImage: fondTetra,
+    },
   ];
 
   return (
@@ -51,7 +96,12 @@ const ArticleList = () => {
               gridColumn: `span ${colSpan}`,
             }}
           >
-            <LargeArticle title={article.title} content={article.content} />
+            <LargeArticle
+              title={article.title}
+              content={article.content}
+              authorName={article.autheur}
+              backgroundImage={article.backgroundImage}
+            />
           </div>
         );
       })}
--- src/components/articlesCards/LargeArticle.jsx
+++ src/components/articlesCards/LargeArticle.jsx
@@ -3,14 +3,30 @@ import { NavLink } from "react-router-dom";
 import "../../css/articlesCards/LargeArticle.css";
 
 const LargeArticle = (props) => {
-  const { id, title, content } = props;
+  const { id, title, content, authorName, authorPhoto, backgroundImage } =
+    props;
 
   return (
-    <div className="card-large">
+    <div
+      className="card-large"
+      style={{ backgroundImage: `url(${backgroundImage})` }}
+    >
       <p>{id}</p>
-      <p>{title}</p>
+      <div className="card-large-gradient"></div>
+      <div className="card-large-content">
+        <div className="card-large-title">{title}</div>
+        <div className="card-large-author">
+          <img
+            src={authorPhoto}
+            alt={authorName}
+            className="card-large-author-photo"
+          />
+          <span className="card-large-author-name">{content}</span>
+        </div>
+      </div>
+      <p></p>
 
-      <p>{content}</p>
+      <p></p>
     </div>
   );
 };
