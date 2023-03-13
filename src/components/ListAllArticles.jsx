fc4a49d3806d01ebcd040c1df066f2dda9d5cf76
Gonni <nicolas.cros.sn@gmail.com>
2023-03-13 20:44:26 +0100

    Ajout page listAllArticle, possibilité de voir plus d'articles si le nombre affiché est > 50

---
+++ src/components/ListAllArticles.jsx
@@ -0,0 +1,101 @@
+import React, { useState } from "react";
+import LargeArticle from "./articlesCards/LargeArticle";
+import "../css/ArticleList.css";
+import "../css/AllArticleList.css";
+import { NavLink } from "react-router-dom";
+import fondTeletubbies from "../img/imgtest/fondwhut.jpg";
+import fondTetra from "../img/imgtest/Fanatique.jpg";
+import fondpasSwitch from "../img/imgtest/passwitch.png";
+import fondTiggle from "../img/imgtest/Tiggle.png";
+import fondCaca from "../img/imgtest/Caca.png";
+import fondMort from "../img/imgtest/Detruit.png";
+
+import PPAmro from "../img/PP/Amro.jpg";
+import PPTez from "../img/PP/Tez.jpg";
+import PPMarnie from "../img/PP/Marnie.jpg";
+
+const AllArticleList = () => {
+  const [NbArticlesAffiche, setNbArticlesAffiche] = useState(40); // initial value of 50
+
+  // On crée une liste d'articles en local
+  const articles = [
+    {
+      id: 1,
+      title: "Analyses et théories sur le dernier épisode des Teletubies",
+      content: "bizarre le type",
+      autheur: "Legonde",
+      ppauteur: PPAmro,
+      backgroundImage: fondTeletubbies,
+    },
+    {
+      id: 2,
+      title: "Officiel: Wind Waker HD sur Nintendo Switch n'existera pas !",
+      content: "Jvais me faire taper moi",
+      autheur: "Askip",
+      ppauteur: PPTez,
+      backgroundImage: fondpasSwitch,
+    },
+    {
+      id: 3,
+      title: "Le jour où je suis devenu le caca de LionRouge",
+      content: "ptdr hein",
+      autheur: "Marnie",
+      ppauteur: PPMarnie,
+      backgroundImage: fondCaca,
+    },
+    {
+      id: 4,
+      title: "Ivre il détruit ses travailleurs",
+      content: "jme suis fait bully",
+      autheur: "Gonni",
+      ppauteur: PPAmro,
+      backgroundImage: fondMort,
+    },
+    {
+      id: 5,
+      title: "Je suis terrifié à l'idée même que Tingle existe",
+      content: "le boug",
+      autheur: "Nebu",
+      ppauteur: PPAmro,
+      backgroundImage: fondTiggle,
+    },
+    {
+      id: 6,
+      title: "Une fanatique de Wind Waker harcèle Nintendo sur Twitter",
+      content: "ça va faire 1 an mdr",
+      autheur: "Lion Rouge",
+      ppauteur: PPAmro,
+      backgroundImage: fondTetra,
+    },
+  ];
+
+  const handleViewMore = () => {
+    setNbArticlesAffiche(NbArticlesAffiche + 10);
+  };
+  return (
+    <div className="global">
+      <div className="article-grid-container">
+        {articles.slice(0, NbArticlesAffiche).map((article) => (
+          <div className="article-grid-item" key={article.id}>
+            <LargeArticle
+              title={article.title}
+              content={article.content}
+              authorName={article.autheur}
+              authorPhoto={article.ppauteur}
+              backgroundImage={article.backgroundImage}
+            />
+          </div>
+        ))}
+      </div>
+      <div
+        className={
+          NbArticlesAffiche > 40 ? "view-more-link" : "Noview-more-link"
+        }
+      >
+        <button onClick={handleViewMore}>Voir plus d'articles</button>
+      </div>
+    </div>
+  );
+};
+
+export default AllArticleList;
--- src/pages/Articles.jsx
+++ src/pages/Articles.jsx
@@ -1,9 +1,31 @@
 import React from "react";
+import "../css/ListeArticles.css";
+import LogoSite from "../img/LogoqueTeznapasfaitLOL.png";
+import { NavLink } from "react-router-dom";
+import AllArticleList from "../components/ListAllArticles";
 
 const Articles = () => {
   return (
-    <div>
-      <h1>Les Articles</h1>
+    <div className="ArticlesListPage">
+      <div className="ImgLogo">
+        <img src={LogoSite} className="LogoSite" />
+      </div>
+
+      <div className="BodySite brandontext-bold-white-16px">
+        <div className="content-Card">
+          <div className="TitleWrapper">
+            <div className="Title montserrat-bold-black-32px">
+              Tous les articles
+            </div>
+            <div className="TitleLine"></div>
+          </div>
+
+          <div className="article-list-wrapper montserrat-bold-black-20px">
+            {/* <ArticleList /> */}
+            <AllArticleList />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
